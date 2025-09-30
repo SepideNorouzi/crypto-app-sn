f2bd715e935b679d152599d779d3c9d5e1f3b09a
Sepide Norouzi <sepidenorouzi17@gmail.com>
2025-09-30 20:26:24 +0330

    first commit

---
+++ src/Services/CryptoApi.js
@@ -0,0 +1,16 @@
+const BASE_URL = "https://api.coingecko.com/api/v3";
+const API_KEY = "CG-cR9GNpJCNiiTMYGeYK67fUyh";
+
+const getCoinList = (page, currency) => {
+  return `${BASE_URL}/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=20&page=${page}&x_cg_demo_api_key=${API_KEY}`;
+};
+
+const SearchCoin = (query) => {
+  return `${BASE_URL}/search?query=${query}&x_cg_demo_api_key=${API_KEY}`;
+};
+
+const marketChart = (coin) => {
+  return `${BASE_URL}/coins/${coin}/market_chart/?vs_currency=usd&days=7`;
+};
+
+export { getCoinList, SearchCoin, marketChart };
---
+++ src/components/modules/Search.jsx
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from "react";
+import { RotatingLines } from "react-loader-spinner";
+
+import { SearchCoin } from "../../Services/CryptoApi";
+
+import styles from "./Search.module.css";
+
+function Search({ currency, setCurrency }) {
+  const [text, setText] = useState("");
+  const [coins, setCoins] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    setCoins([]);
+    if (!text) {
+      setIsLoading(false);
+      return;
+    }
+
+    const Search = async () => {
+      try {
+        const res = await fetch(SearchCoin(text), {
+          signal: controller.signal,
+        });
+        const json = await res.json();
+        console.log(json);
+        if (json.coins) {
+          setIsLoading(false);
+          setCoins(json.coins);
+        } else {
+          alert(json.status.error_message);
+        }
+      } catch (error) {
+        if (error.name !== "AbortError") {
+          alert(error.message);
+        }
+      }
+    };
+
+    setIsLoading(true);
+    Search();
+
+    return () => controller.abort();
+  }, [text]);
+
+  return (
+    <div className={styles.searchBox}>
+      <input
+        type="text"
+        placeholder="Search"
+        text={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <select value={currency} onChange={(e) => setCurrency(e.target.value)}>
+        <option value="usd">USD</option>
+        <option value="eur">EUR</option>
+        <option value="jpy">JPY</option>
+      </select>
+      {(!!coins.length || isLoading) && (
+        <div className={styles.searchResult}>
+          {isLoading && (
+            <RotatingLines
+              width="50px"
+              height="50px"
+              strokeWidth="2"
+              strokeColor="#3874ff"
+            />
+          )}
+          <ul>
+            {coins.map((coin) => (
+              <li key={coin.id}>
+                <img src={coin.thumb} alt={coin.name} />
+                <p>{coin.name}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Search;
